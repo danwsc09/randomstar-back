c31777096d665ec88ba5b77885a14de1dfd38fc4
Daniel Chong <temporaryaion1@hotmail.com>
2021-10-17 21:59:38 +0900

    test: initialize test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,5 +2,5 @@ import { abilitiesRouter } from "./abilities";
 import * as core from "express-serve-static-core";
 
 export const mountRoutes = (app: core.Express) => {
-  app.use("/abilities", abilitiesRouter);
+  app.use("/api/abilities", abilitiesRouter);
 };
